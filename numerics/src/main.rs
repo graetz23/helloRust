b7e3b7fd11ba8b3a10d65bf5909e5bfccf89f699
graetz23 <graetz23@gmail.com>
2024-10-19 20:31:52 +0200

    adding all arithmetic operations ..

--- numerics/src/main.rs
+++ numerics/src/main.rs
@@ -4,7 +4,19 @@
 //const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
 fn main() {
-  let x = 2.0; // f64
+    // addition
+    let sum = 5 + 10;
 
-  let y: f32 = 3.0; // f32
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // results in -1
+
+    // remainder
+    let remainder = 43 % 5;
 } // main
