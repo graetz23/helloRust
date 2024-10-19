4b39b2b1a6b5bfafcfa5dc34569dd2550dfd364f
graetz23 <graetz23@gmail.com>
2024-10-19 20:39:55 +0200

    adding reading out tuple values separately ..

--- numerics/src/main.rs
+++ numerics/src/main.rs
@@ -8,6 +8,10 @@ fn main() {
     let tup : (i32, f64, u8) = (500, 6.4, 1);
     let (x, y, z) = tup;
 
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+
     // character
     let c = 'z'; // implicit
     let z : char = 'z'; // with explicit type annotation
